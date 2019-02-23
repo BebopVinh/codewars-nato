c4e11a3bd2b4d2b03363466b0ad32ba24a56410e
Vinh Do <vinhdo07@gmail.com>
2019-02-23 06:44:49 +0000

    Initial solution, contains Exit Code: 1

---
+++ sandbox.rb
@@ -0,0 +1,15 @@
+ALPHABET = %w(Alpha Bravo Charlie Delta Echo Foxtrot Golf Hotel India Juliet Kilo Lima Mike November Oscar Papa Quebec Romeo Sierra Tango Uniform Victor Whiskey XRay Yankee Zulu Zero One Two Tree Four Fife Six Seven Eight Niner)
+
+def nato(message)
+  array = message.chars
+  answer = []
+  array.each do |character|
+    if !!(character.match(/\d/))
+      answer << ALPHABET[26..35].find {|word| ALPHABET.index(word) - 26 == character.to_i}
+    else
+      answer << ALPHABET[0..25].find {|word| word[0] == character.upcase}
+    end
+  end
+  answer.reject!(&:empty?)
+  answer.join(" ")
+end
